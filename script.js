927a24bb7c6bc266442aced01aae1715bd8cd50f
Foggles <jackfogarty2105@gmail.com>
2020-11-25 10:44:11 +0800

    Added dynamic slider to allow user to select password length from 8 -> 128

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var slider = document.getElementById("PasswordLength");
+var output = document.getElementById("sliderDisplay");
+
+output.innerHTML = slider.value; // Display the default slider value
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 // Write password to the #password input
 function writePassword() {
