0c3b0d6a9082ae32ac19ff502a683521767fc007
Foggles <jackfogarty2105@gmail.com>
2020-11-25 11:01:48 +0800

    added arrays containing lowercase letters, uppercase letters, numbers and special characters

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var slider = document.getElementById("PasswordLength");
-var output = document.getElementById("sliderDisplay");
+let generateBtn = document.querySelector("#generate");
+let slider = document.getElementById("PasswordLength");
+let output = document.getElementById("sliderDisplay");
 
 output.innerHTML = slider.value; // Display the default slider value
 
@@ -9,10 +9,18 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
+// Arrays for lowercase, uppercase, numbers & special characters
+let lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let specArr = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
